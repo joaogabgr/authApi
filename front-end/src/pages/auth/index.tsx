3a60f6215d7ed3a89c1ce6374cc1ea2a96bfe921
João Gabriel <joaoggbs62@gmail.com>
2024-06-22 20:13:39 -0300

    [FRONT] style: Melhorando o estilo da pagina de autenticação

--- front-end/src/pages/auth/index.tsx
+++ front-end/src/pages/auth/index.tsx
@@ -15,6 +15,9 @@ export default function Auth() {
       const response = await axios.post('http://localhost:5000/users/userlogin', { userUser: email, userPassword: password });
       localStorage.setItem('token', response.data.success.token);
       successSwal('Usuário logado com sucesso');
+      setTimeout(() => {
+        window.location.href = '/private';
+      }, 1000);
     } catch (error) {
       if (axios.isAxiosError(error)) {
         if (error.response) {
@@ -32,6 +35,7 @@ export default function Auth() {
     const form = event.currentTarget;
     const cpf = form.cpf.value;
     const email = form.email.value;
+    const telefone = form.telefone.value;
     const usuario = form.usuario.value;
     const nome = form.nome.value;
     const password = form.password.value;
@@ -46,7 +50,7 @@ export default function Auth() {
       const response = await axios.post('http://localhost:5000/users/userCreate', {
         userCpf: cpf,
         userEmail: email,
-        userPhone: '',
+        userPhone: telefone,
         userUser: usuario,
         userName: nome,
         userPassword: password,
@@ -66,9 +70,21 @@ export default function Auth() {
     }
   };
 
+  const ChangeHidden = (event: React.MouseEvent<HTMLParagraphElement>) => {
+    const login = document.querySelector('.Login');
+    const cadastro = document.querySelector('.Cadastro');
+    if (login?.classList.contains('hidden')) {
+      login?.classList.remove('hidden');
+      cadastro?.classList.add('hidden');
+    } else {
+      login?.classList.add('hidden');
+      cadastro?.classList.remove('hidden');
+    }
+  }
+
   return (
     <main>
-      <section className='Login'>
+      <section className='formAuth Login'>
         <h1>Login</h1>
         <form onSubmit={handleSubmitLogin}>
           <label htmlFor='email'>Email</label>
@@ -77,8 +93,9 @@ export default function Auth() {
           <input type='password' id='password' name='password' required />
           <button type='submit'>Login</button>
         </form>
+        <p onClick={ChangeHidden} className='loginHidden'>Não tem uma conta? clique aqui!</p>
       </section>
-      <section className='Cadastro'>
+      <section className='formAuth Cadastro hidden'>
         <h1>Cadastro</h1>
         <form onSubmit={handleSubmitCadastro}>
           <label htmlFor='cpf'>CPF:</label>
@@ -87,6 +104,8 @@ export default function Auth() {
           <input type='email' id='email' name='email' required />
           <label htmlFor='usuario'>Usuario</label>
           <input type='text' id='usuario' name='usuario' required />
+          <label htmlFor='telefone'>Telefone</label>
+          <input type='tel' id='telefone' name='telefone' required />
           <label htmlFor='nome'>Nome</label>
           <input type='text' id='nome' name='nome' required />
           <label htmlFor='password'>Senha</label>
@@ -95,6 +114,7 @@ export default function Auth() {
           <input type='password' id='password2' name='password2' required />
           <button type='submit'>Cadastrar</button>
         </form>
+        <p onClick={ChangeHidden} className='cadastroHidden'>Já tem uma conta? clique aqui!</p>
       </section>
     </main>
   );
